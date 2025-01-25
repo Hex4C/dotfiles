ba0c8d0456fec7762ab71b71bdf8438bef90e828
Mikroskoldis <58385827+Mikroskoldis@users.noreply.github.com>
2025-01-25 18:37:16 +0100

    feat: Add zen-mode.nvim
    Add zen mode which can be toggled on and off with :ZenMode to avoid
    having to open the sidebar to center the code and make it easier to
    solely focus on a certain part of the code.
    
    zen-mode.nvim made by Folke.

--- init.lua
+++ init.lua
@@ -760,6 +760,39 @@ require('lazy').setup({
     opts = { signs = false },
   },
 
+  {
+    'folke/zen-mode.nvim',
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+      options = {
+        number = true,
+      },
+      plugin = {
+        options = {
+          enabled = true,
+          ruler = false, -- disables the ruler text in the cmd line area
+          showcmd = false, -- disables the command in the last line of the screen
+          -- you may turn on/off statusline in zen mode by setting 'laststatus'
+          -- statusline will be shown only if 'laststatus' == 3
+          laststatus = 0, -- turn off the statusline in zen mode
+        },
+        gitsigns = { enabled = true }, -- disables git signs
+        tmux = { enabled = false }, -- disables the tmux statusline
+        todo = { enabled = false }, -- if set to "true", todo-comments.nvim highlights will be disabled
+
+        -- this will change the font size on alacritty when in zen mode
+        -- requires  Alacritty Version 0.10.0 or higher
+        -- uses `alacritty msg` subcommand to change font size
+        alacritty = {
+          enabled = false,
+          font = '14', -- font size
+        },
+      },
+    },
+  },
+
   -- Markdown preview, opens a webbrowser on localhost to render and preview the current markdown
   -- document if the preview is started.
   {
