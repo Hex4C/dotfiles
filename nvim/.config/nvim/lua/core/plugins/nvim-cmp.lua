13e9115305a0816029e1c9157faa371dcb93aa07
Hex4C <58385827+Hex4C@users.noreply.github.com>
2025-03-05 14:46:12 +0100

    feat: Add lsp signature help and comment out buffer cmp

--- nvim/.config/nvim/lua/core/plugins/nvim-cmp.lua
+++ nvim/.config/nvim/lua/core/plugins/nvim-cmp.lua
@@ -31,8 +31,9 @@ return { -- Autocompletion
     --  nvim-cmp does not ship with all sources by default. They are split
     --  into multiple repos for maintenance purposes.
     'hrsh7th/cmp-nvim-lsp',
-    'hrsh7th/cmp-buffer',
+    -- 'hrsh7th/cmp-buffer',
     'hrsh7th/cmp-path',
+    'hrsh7th/cmp-nvim-lsp-signature-help',
   },
   config = function()
     -- See `:help cmp`
