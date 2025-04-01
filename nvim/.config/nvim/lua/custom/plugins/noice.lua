ac510de7919ea881dd633f08bd1396df043dc6a6
Hex4C <58385827+Hex4C@users.noreply.github.com>
2025-04-01 20:12:56 +0200

    style(noice): Move dependecy code higher up in def

--- nvim/.config/nvim/lua/custom/plugins/noice.lua
+++ nvim/.config/nvim/lua/custom/plugins/noice.lua
@@ -4,6 +4,14 @@ return {
   opts = {
     -- add any options here
   },
+  dependencies = {
+    -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+    'MunifTanjim/nui.nvim',
+    -- OPTIONAL:
+    --   `nvim-notify` is only needed, if you want to use the notification view.
+    --   If not available, we use `mini` as the fallback
+    'rcarriga/nvim-notify',
+  },
   config = function()
     -- NOTE: Potentially limit the size of notify view.
 
@@ -41,12 +49,4 @@ return {
       },
     }
   end,
-  dependencies = {
-    -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
-    'MunifTanjim/nui.nvim',
-    -- OPTIONAL:
-    --   `nvim-notify` is only needed, if you want to use the notification view.
-    --   If not available, we use `mini` as the fallback
-    'rcarriga/nvim-notify',
-  },
 }
