62f133227fb9063010878b3d4ccde86cf2d1bb66
Hex4C <58385827+Hex4C@users.noreply.github.com>
2025-04-01 19:55:10 +0200

    bugfix(telescope): Bump telescope to rely on master branch

--- nvim/.config/nvim/lua/core/plugins/telescope.lua
+++ nvim/.config/nvim/lua/core/plugins/telescope.lua
@@ -1,7 +1,9 @@
 return { -- Fuzzy Finder (files, lsp, etc)
   'nvim-telescope/telescope.nvim',
   event = 'VimEnter',
-  branch = '0.1.x',
+  -- branch = '0.1.x',
+  branch = 'master',
+  -- version = false,
   dependencies = {
     'nvim-lua/plenary.nvim',
     { -- If encountering errors, see telescope-fzf-native README for installation instructions
