82de338cabd032c159893d7de23a599ec7874aad
Hex4C <58385827+Hex4C@users.noreply.github.com>
2025-08-14 23:49:39 +0200

    feat(oil-nvim): Add oil for more robust file editing
    Rocking double file editors, oil looks promising and currently bound to
    the - keybind, might introduce a secondary bind \ which works good on
    windows.
    
    Keeping neotree for the sidebar and more overview of the current file
    structure and perhaps creating a single new file quickly and oil for
    more robust, creating several files, moving etc...

---
+++ nvim/.config/nvim/lua/custom/plugins/oil.lua
@@ -0,0 +1,25 @@
+return {
+  'stevearc/oil.nvim',
+  ---@module 'oil'
+  ---@type oil.SetupOpts
+  opts = {},
+  -- Optional dependencies
+  dependencies = { { 'echasnovski/mini.icons', opts = {} } },
+  -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
+  -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
+  lazy = false,
+  config = function()
+    vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open parent directory' })
+    require('oil').setup {
+      delete_to_trash = false,
+      default_file_explorer = false,
+      columns = {
+        'icon',
+        'size',
+      },
+      view_options = {
+        show_hidden = true,
+      },
+    }
+  end,
+}
