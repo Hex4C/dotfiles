2a81dacc2a8d81cc082a84fcdc857a8851fb5500
Hex4C <58385827+Hex4C@users.noreply.github.com>
2025-08-28 22:25:00 +0200

    feat(nvim-debogger): Add nvim debugger
    Configure and add codelldb and debugpy as ensure_installed.

--- nvim/.config/nvim/lua/lazy-plugins.lua
+++ nvim/.config/nvim/lua/lazy-plugins.lua
@@ -64,7 +64,7 @@ require('lazy').setup({
   require 'core.plugins.neo-tree',
 
   -- TODO: Import the debogger here later
-  -- require 'core.plugins.debug',
+  require 'core.plugins.debug',
 
   --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
   --    For additional information, see `:help lazy.nvim-lazy.nvim-structuring-your-plugins`
