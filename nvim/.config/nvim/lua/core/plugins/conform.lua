2ea911ab9197aed543187737b2cd31dba80b5b93
Hex4C <58385827+Hex4C@users.noreply.github.com>
2025-09-18 22:43:00 +0200

    feat(nvim-conform): Add prettier to fire for css and vue files

--- nvim/.config/nvim/lua/core/plugins/conform.lua
+++ nvim/.config/nvim/lua/core/plugins/conform.lua
@@ -37,6 +37,8 @@ return {
       svelte = { 'prettier' },
       typescript = { 'prettier' },
       javascriptreact = { 'prettier' },
+      vue = { 'prettier' },
+      css = { 'prettier' },
       --
       -- You can use 'stop_after_first' to run the first available formatter from the list
       javascript = { 'prettier', stop_after_first = true },
