509ed2232626d3dc270e146d0ddb2697d336403d
Hex4C <58385827+Hex4C@users.noreply.github.com>
2025-10-01 22:42:30 +0200

    fix(nvim-debug): Adjust nvim dap to work lazyload properly
    Yay

--- nvim/.config/nvim/lua/core/plugins/debug.lua
+++ nvim/.config/nvim/lua/core/plugins/debug.lua
@@ -8,59 +8,37 @@
 
 return {
   'mfussenegger/nvim-dap',
+  event = 'VeryLazy',
   dependencies = {
-    -- Creates a beautiful debugger UI
     'rcarriga/nvim-dap-ui',
+    -- 'theHamsta/nvim-dap-virtual-text',
     'nvim-neotest/nvim-nio',
-    'mason-org/mason.nvim',
+    'williamboman/mason.nvim',
     'jay-babu/mason-nvim-dap.nvim',
-    -- Add your own debuggers here
-    'leoluz/nvim-dap-go',
   },
-  cmd = { 'DapClearBreakpoints', 'DapNew', 'DapInstall', 'DapUninstall', 'DapToggleBreakpoint', 'DapShowLog' },
-  keys = function(_, keys)
-    local dap = require 'dap'
-    local dapui = require 'dapui'
-    return {
-      -- Basic debugging keymaps, feel free to change to your liking!
-      { '<F5>', dap.continue, desc = 'Debug: Start/Continue' },
-      { '<F1>', dap.step_into, desc = 'Debug: Step Into' },
-      { '<F2>', dap.step_over, desc = 'Debug: Step Over' },
-      { '<F3>', dap.step_out, desc = 'Debug: Step Out' },
-      { '<leader>b', dap.toggle_breakpoint, desc = 'Debug: Toggle Breakpoint' },
-      {
-        '<leader>B',
-        function()
-          dap.set_breakpoint(vim.fn.input 'Breakpoint condition: ')
-        end,
-        desc = 'Debug: Set Breakpoint',
-      },
-      -- Toggle to see last session result. Without this, you can't see session output in case of unhandled exception.
-      { '<F7>', dapui.toggle, desc = 'Debug: See last session result.' },
-      unpack(keys),
-    }
-  end,
+  -- cmd = { 'DapClearBreakpoints', 'DapNew', 'DapInstall', 'DapUninstall', 'DapToggleBreakpoint', 'DapShowLog' },
   config = function()
     local dap = require 'dap'
     local dapui = require 'dapui'
 
+    vim.keymap.set('n', '<F1>', dap.continue, { desc = 'Debug: Start/Continue' })
+    vim.keymap.set('n', '<F2>', dap.step_into, { desc = 'Debug: Step Into' })
+    vim.keymap.set('n', '<F3>', dap.step_over, { desc = 'Debug: Step Over' })
+    vim.keymap.set('n', '<F4>', dap.step_out, { desc = 'Debug: Step Out' })
+    -- vim.keymap.set('n', '<F5>', dap.step_back)
+    vim.keymap.set('n', '<F6>', dap.terminate, { desc = 'Debug: Terminate' })
+
+    vim.fn.sign_define('DapBreakpoint', { text = '', texthl = 'DiagnosticSignError', linehl = '', numhl = '' })
+    vim.keymap.set('n', '<space>b', dap.toggle_breakpoint, { desc = 'Debug: Toggle Breakpoint' })
+    vim.keymap.set('n', '<space>gb', dap.run_to_cursor, { desc = 'Debug: Run to cursor' })
+
     require('mason-nvim-dap').setup {
-      -- Makes a best effort to setup the various debuggers with
-      -- reasonable debug configurations
       automatic_installation = true,
-
-      -- You can provide additional configuration to the handlers,
-      -- see mason-nvim-dap README for more information
-      handlers = {},
-
-      -- You'll need to check that you have the required things installed
-      -- online, please don't ask me how to install them :)
       ensure_installed = {
-        -- Update this to ensure that you have the debuggers for the langs you want
-        'delve',
         'codelldb',
         'debugpy',
       },
+      handlers = {},
     }
 
     -- Dap UI setup
@@ -84,18 +62,17 @@ return {
         },
       },
     }
-
-    dap.listeners.after.event_initialized['dapui_config'] = dapui.open
-    dap.listeners.before.event_terminated['dapui_config'] = dapui.close
-    dap.listeners.before.event_exited['dapui_config'] = dapui.close
-
-    -- Install golang specific config
-    require('dap-go').setup {
-      delve = {
-        -- On Windows delve must be run attached or it crashes.
-        -- See https://github.com/leoluz/nvim-dap-go/blob/main/README.md#configuring
-        detached = vim.fn.has 'win32' == 0,
-      },
-    }
+    dap.listeners.before.attach.dapui_config = function()
+      dapui.open()
+    end
+    dap.listeners.before.launch.dapui_config = function()
+      dapui.open()
+    end
+    dap.listeners.before.event_terminated.dapui_config = function()
+      dapui.close()
+    end
+    dap.listeners.before.event_exited.dapui_config = function()
+      dapui.close()
+    end
   end,
 }
