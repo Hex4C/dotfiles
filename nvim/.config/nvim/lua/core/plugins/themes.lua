36d2321c64cfdecb879d1d686fac23c6ba5dc76a
Hex4C <58385827+Hex4C@users.noreply.github.com>
2025-10-20 21:10:53 +0200

    chore(nvim-theme): Increase the prio on the theme a little bit
    Shouldn't matter too much, just future proofing

--- nvim/.config/nvim/lua/core/plugins/themes.lua
+++ nvim/.config/nvim/lua/core/plugins/themes.lua
@@ -1,7 +1,7 @@
 return {
   'catppuccin/nvim',
   name = 'catppuccin',
-  priority = 10,
+  priority = 1000,
   init = function()
     vim.cmd.colorscheme 'catppuccin-mocha'
     vim.cmd.hi 'Comment gui=none'
