d4b66803a96655ae6ebae050fa689567d8b4450f
Hex4C <58385827+Hex4C@users.noreply.github.com>
2026-04-26 00:05:19 +0200

    fix: Add regex for noice to install on new build

--- nvim/.config/nvim/lua/core/plugins/nvim-treesitter.lua
+++ nvim/.config/nvim/lua/core/plugins/nvim-treesitter.lua
@@ -9,7 +9,7 @@ return {
     branch = 'main',
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter-intro`
     config = function()
-      local parsers = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', 'python', 'rust', 'go' }
+      local parsers = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', 'python', 'rust', 'go', 'regex' }
       require('nvim-treesitter').install(parsers)
       vim.api.nvim_create_autocmd('FileType', {
         callback = function(args)
