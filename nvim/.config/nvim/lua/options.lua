acb3ddedb891d26add90ae785864eb0345a73524
Hex4C <58385827+Hex4C@users.noreply.github.com>
2026-06-07 00:48:52 +0200

    fix: Jumpy nvim numbers when moving from 9 to 10 loc

--- nvim/.config/nvim/lua/options.lua
+++ nvim/.config/nvim/lua/options.lua
@@ -15,6 +15,7 @@ vim.opt.number = true
 -- You can also add relative line numbers, to help with jumping.
 --  Experiment for yourself to see if you like it!
 vim.opt.relativenumber = true
+vim.wo.numberwidth = 5
 
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.opt.mouse = 'a'
