4adcc02480d288e871ac262b017cf0c9476df92f
Hex4C <58385827+Hex4C@users.noreply.github.com>
2026-08-31 13:01:07 +0200

    feat(nvim): update word grep to use new api

--- nvim/.config/nvim/lua/core/plugins/fff.lua
+++ nvim/.config/nvim/lua/core/plugins/fff.lua
@@ -42,7 +42,7 @@ return {
     -- 1. Files & Grep (Handled brilliantly by fff)
     vim.keymap.set('n', '<leader>sf', function() fff.find_files() end, { desc = '[S]earch [F]iles' })
     vim.keymap.set('n', '<leader>sg', function() fff.live_grep() end, { desc = '[S]earch by [G]rep' })
-    vim.keymap.set('n', '<leader>sw', function() fff.live_grep { query = vim.fn.expand '<cword>' } end, { desc = '[S]earch current [W]ord' })
+    vim.keymap.set('n', '<leader>sw', function() fff.live_grep_under_cursor() end, { desc = '[S]earch current [W]ord' })
 
     -- Search Neovim configuration directory using fff
     vim.keymap.set('n', '<leader>sn', function() fff.find_files { cwd = vim.fn.stdpath 'config' } end, { desc = '[S]earch [N]eovim files' })
